c7f844ef970cb11940fde0a7cece7cbb2ec595dd
videomaker318 <autismhitman@gmail.com>
2024-02-08 13:06:02 +0530

    first

---
+++ src/test/java/sampleBuild/A.java
@@ -0,0 +1,20 @@
+package sampleBuild;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class A {
+ 
+	
+	
+    @Test
+	public void mA() {
+    
+		System.out.println("Started A");
+		
+		Assert.assertTrue(true);
+
+	}
+
+}
